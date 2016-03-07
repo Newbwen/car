aee2149e8c09f5ed250e1b85c3a87ec3b2ac7b3b
lvdi <294390535@qq.com>
2016-03-07 17:06:25 +0800

    车辆增加修改查询，评价

--- src/main/java/pengyi/repository/evaluate/EvaluateResposition.java
+++ src/main/java/pengyi/repository/evaluate/EvaluateResposition.java
@@ -7,8 +7,8 @@ import pengyi.domain.model.user.IUserRepository;
 import pengyi.repository.generic.AbstractHibernateGenericRepository;
 
 /**
- * Author: pengyi
- * Date: 15-12-30
+ * Author: lvdi
+ * Date: 15-3-7
  */
 @Repository("evaluateRepository")
 public class EvaluateResposition extends AbstractHibernateGenericRepository<BaseUser, String> implements IEvaluateRepository<BaseUser, String> {
