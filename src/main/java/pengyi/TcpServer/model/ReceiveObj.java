97a9d72b2e971041c8007a0bf12dca1d79037e28
pengyi <pengyi9627@gmail.com>
2016-03-10 13:58:01 +0800

    tcp初始化

---
+++ src/main/java/pengyi/TcpServer/model/ReceiveObj.java
@@ -0,0 +1,35 @@
+package pengyi.TcpServer.model;
+
+/**
+ * 接收客户端
+ * Created by pengyi on 2016/3/10.
+ */
+public class ReceiveObj {
+
+    private int type;               //1为连接服务器，2为下单，3为接单
+    private String content;         //类容
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public ReceiveObj() {
+    }
+
+    public ReceiveObj(int type, String content) {
+        this.type = type;
+        this.content = content;
+    }
+}
