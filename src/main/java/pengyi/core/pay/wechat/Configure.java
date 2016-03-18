c2952e85d06c2d7dd4e35ac5c3f45602434d4c29
pengyi <pengyi9627@gmail.com>
2016-03-18 16:36:18 +0800

    Merge remote-tracking branch 'remotes/origin/lvdi'
    # Conflicts:
    #	src/main/java/pengyi/core/api/ResponseMessage.java

--- src/main/java/pengyi/core/pay/wechat/Configure.java
+++ src/main/java/pengyi/core/pay/wechat/Configure.java
@@ -6,7 +6,7 @@ package pengyi.core.pay.wechat;
 public class Configure {
 
     public static String key = "";          //微信支付key
-    public static String appid = "";        //公众号id
+    public static String appid = "";        //appid
     public static String mch_id="";         //商户id
 
     public static String unified_url = "https://api.mch.weixin.qq.com/pay/unifiedorder";
