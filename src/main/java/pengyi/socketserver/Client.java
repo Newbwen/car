57f468b87388df47bbbcb822c9f974a55c7c1385
pengyi <pengyi9627@gmail.com>
2016-03-25 11:49:01 +0800

    推送初始化，接单推送用户

---
+++ src/main/java/pengyi/socketserver/Client.java
@@ -0,0 +1,108 @@
+package pengyi.socketserver;
+
+import com.alibaba.fastjson.JSON;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+import pengyi.core.type.UserType;
+import pengyi.socketserver.model.ReceiveObj;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Locale;
+
+/**
+ * Created by pengyi on 2016/3/25.
+ */
+@Component
+public class Client implements Runnable {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private Socket s;
+    private String phone;
+    private UserType userType;
+    private DataInputStream dis = null;
+    private DataOutputStream dos = null;
+    private boolean bConnected = false;
+
+    @Autowired
+    private MessageSource messageSource;
+
+    public Client(Socket s) {
+        this.s = s;
+        try {
+            dis = new DataInputStream(s.getInputStream());
+            dos = new DataOutputStream(s.getOutputStream());
+            bConnected = true;
+        } catch (IOException e) {
+            logger.info(messageSource.getMessage("socket.connection.fail.message", new Object[]{"unknow", "UNKNOW"}, Locale.CHINA));
+            close();
+            e.printStackTrace();
+        }
+    }
+
+    public void send(String str) {
+        try {
+            dos.writeUTF(str);
+        } catch (IOException e) {
+            logger.info(messageSource.getMessage("socket.server.sendMessage.fail.message", new Object[]{phone, userType}, Locale.CHINA));
+            close();
+        }
+    }
+
+    public void close() {
+        switch (userType) {
+            case USER:
+                TcpService.userClients.remove(phone);
+                break;
+            case DRIVER:
+                TcpService.driverClients.remove(phone);
+                break;
+        }
+
+        try {
+            if (dis != null)
+                dis.close();
+            if (dos != null)
+                dos.close();
+            if (s != null) {
+                s.close();
+            }
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    public void run() {
+        try {
+            while (bConnected) {
+                String str = dis.readUTF();
+                dos.writeUTF(str);
+                ReceiveObj obj = JSON.parseObject(str, ReceiveObj.class);
+                phone = obj.getPhone();
+                userType = obj.getType();
+                switch (obj.getType()) {
+                    case USER:
+                        TcpService.userClients.put(phone, this);
+                        break;
+                    case DRIVER:
+                        TcpService.driverClients.put(phone, this);
+                        break;
+                }
+            }
+        } catch (EOFException e) {
+            logger.info(messageSource.getMessage("socket.shutdown.message", new Object[]{phone, userType}, Locale.CHINA));
+            e.printStackTrace();
+        } catch (IOException e) {
+            logger.info(messageSource.getMessage("socket.dirty.shutdown.message", new Object[]{phone, userType}, Locale.CHINA));
+            e.printStackTrace();
+        } finally {
+            close();
+        }
+    }
+}
--- src/main/java/pengyi/socketserver/TcpService.java
+++ src/main/java/pengyi/socketserver/TcpService.java
@@ -1,13 +1,15 @@
 package pengyi.socketserver;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.EOFException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import java.io.IOException;
 import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 /**
@@ -17,7 +19,12 @@ public class TcpService implements Runnable {
 
     private ServerSocket serverSocket;
     boolean started = false;
-    Map<String, Client> clients = new HashMap<String, Client>();
+    public static Map<String, Client> userClients = new HashMap<String, Client>();
+    public static Map<String, Client> driverClients = new HashMap<String, Client>();
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private MessageSource messageSource;
 
     @Override
     public void run() {
@@ -28,9 +35,11 @@ public class TcpService implements Runnable {
                 serverSocket = new ServerSocket(port);
                 started = true;
                 used = false;
+                logger.info(messageSource.getMessage("socket.open.success.message", new Object[]{port}, Locale.CHINA));
             } catch (BindException e) {
                 port++;
             } catch (IOException e) {
+                logger.error("socket.open.fail.message");
                 e.printStackTrace();
                 used = false;
             }
@@ -43,74 +52,13 @@ public class TcpService implements Runnable {
                 new Thread(c).start();
             }
         } catch (IOException e) {
+            logger.error("socket.server.dirty.shutdown.message");
             e.printStackTrace();
         } finally {
             try {
                 serverSocket.close();
             } catch (IOException e) {
                 e.printStackTrace();
-            }
-        }
-    }
-
-    class Client implements Runnable {
-        private Socket s;
-        private String userId;
-        private DataInputStream dis = null;
-        private DataOutputStream dos = null;
-        private boolean bConnected = false;
-
-        public Client(Socket s) {
-            this.s = s;
-            try {
-                dis = new DataInputStream(s.getInputStream());
-                dos = new DataOutputStream(s.getOutputStream());
-                bConnected = true;
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        public void send(String str) {
-            try {
-                dos.writeUTF(str);
-            } catch (IOException e) {
-                clients.remove(userId);
-            }
-        }
-
-        public void run() {
-            try {
-                while (bConnected) {
-                    String str = dis.readUTF();
-                    dos.writeUTF(str);
-//                    ReceiveObj obj = JSON.parseObject(str, ReceiveObj.class);
-//                    switch (obj.getType()) {
-//                        case 1:
-//                            userId = obj.getContent();
-//                            clients.put(userId, this);
-//                            break;
-//                    }
-                }
-            } catch (EOFException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                clients.remove(userId);
-                try {
-                    if (dis != null)
-                        dis.close();
-                    if (dos != null)
-                        dos.close();
-                    if (s != null) {
-                        s.close();
-                    }
-
-                } catch (IOException e1) {
-                    e1.printStackTrace();
-                }
-
             }
         }
     }
