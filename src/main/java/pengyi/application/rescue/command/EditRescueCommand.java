683c5a25ba9f04b9b11ea33caabd50a1ed9722b7
pengyi <pengyi9627@gmail.com>
2016-03-29 11:26:04 +0800

    Merge remote-tracking branch 'remotes/origin/lvdi'
    # Conflicts:
    #	src/main/resources/config.properties

--- src/main/java/pengyi/application/rescue/command/EditRescueCommand.java
+++ src/main/java/pengyi/application/rescue/command/EditRescueCommand.java
@@ -13,19 +13,19 @@ public class EditRescueCommand {
     private String id;
     private Integer version;
 
-    @NotEmpty(message = "{rescue.applyUser,NotEmpty,message}")
+    @NotEmpty(message = "{rescue.applyUser.NotEmpty.message}")
     private String applyUser;                 //申请人
 
-    @NotNull(message = "{rescue.type,Notnull,message}")
+    @NotNull(message = "{rescue.type.Notnull.message}")
     private RescueType type;                           //救援类型
 
-    @NotEmpty(message = "{rescue.description,NotEmpty,message}")
+    @NotEmpty(message = "{rescue.description.NotEmpty.message}")
     private String description;                 //救援说明
 
-    @NotEmpty(message = "{rescue.driver,NotEmpty,message}")
+    @NotEmpty(message = "{rescue.driver.NotEmpty.message}")
     private String driver;                      //救援司机
 
-    @NotEmpty(message = "{rescue.rescueStatus,Notnull,message}")
+    @NotEmpty(message = "{rescue.rescueStatus.Notnull.message}")
     private RescueStatus rescueStatus;                         //救援状态（1待救援、2救援中、3已救援）
 
     public RescueStatus getRescueStatus() {
